8c3c7a450906623c8494856a2abc9e82307aaed4
toooooof <christophe.denoize@free.fr>
2016-04-21 17:45:40 +0200

    Basic test file

---
+++ src/model.js
@@ -0,0 +1,22 @@
+(function(BGC) {
+
+	BGC.Model = function(options) {
+
+	};
+
+	BGC.Model.import = function(str) {
+
+	};
+
+	(function() {
+	    this.log = function(action, timestamp, player, parameters) {
+	        
+	    };
+	    this.export = function() {
+	        
+	    };
+	}).call(BGC.Model.prototype);
+
+	window.BGC = BGC;
+
+})(window.BGC || {});
